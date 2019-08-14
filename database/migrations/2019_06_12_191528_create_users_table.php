742073e98fc355687efeaae5f818cba6a2b7fa7f
ezegy fa <darvaswow@yahoo.com>
2019-08-14 18:19:41 +0300

    Updating database

--- database/migrations/2019_06_12_191528_create_users_table.php
+++ database/migrations/2019_06_12_191528_create_users_table.php
@@ -18,15 +18,11 @@ class CreateUsersTable extends Migration {
 			$table->string('first_name', 191);
 			$table->string('last_name', 191);
 			$table->string('email', 191)->unique('buyer_email_unique');
-			$table->boolean('verified')->default(false)->nullable();
+			$table->boolean('verified')->default(0);
 			$table->string('password', 191);
 			$table->string('phone_number', 10)->nullable();
 			$table->text('address', 65535)->nullable();
 			$table->integer('location_id')->nullable()->index('buyer_location');
-			$table->integer('identity_card_serial_type_id')->nullable()->index('fkIdx_486');
-			$table->string('identity_card_serial_number', 6)->nullable();
-			$table->boolean('accepted_gdpr')->nullable();
-			$table->boolean('isAnonymized')->default(0);
 			$table->string('remember_token', 100)->nullable();
 			$table->dateTime('last_activity')->nullable();
 			$table->timestamps();
--- database/migrations/2019_06_12_191530_add_foreign_keys_to_users_table.php
+++ database/migrations/2019_06_12_191530_add_foreign_keys_to_users_table.php
@@ -14,7 +14,6 @@ class AddForeignKeysToUsersTable extends Migration {
 	{
 		Schema::table('users', function(Blueprint $table)
 		{
-			$table->foreign('identity_card_serial_type_id', 'FK_486')->references('id')->on('identity_card_serial_types')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 			$table->foreign('location_id', 'buyer_location')->references('id')->on('settlements')->onUpdate('RESTRICT')->onDelete('RESTRICT');
 		});
 	}
@@ -29,7 +28,6 @@ class AddForeignKeysToUsersTable extends Migration {
 	{
 		Schema::table('users', function(Blueprint $table)
 		{
-			$table->dropForeign('FK_486');
 			$table->dropForeign('buyer_location');
 		});
 	}
